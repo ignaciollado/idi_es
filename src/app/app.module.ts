9171f578254610e8e2a68b5cede5c658671423fb
ignacio.llado@idi.es <ignacio.llado@idi.es>
2023-08-23 13:31:38 +0200

    he modificado el footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,17 +13,16 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
-//import { FeaturedIdiComponent } from './featured-idi/featured-idi.component';
+
 import { AgendaIdiComponent } from './agenda-idi/agenda-idi.component';
-//import { ServicesIdiComponent } from './services-idi/services-idi.component';
-//import { FeaturedIdiDetailComponent } from './featured-idi-detail/featured-idi-detail.component';
+
 import { ServiceIdiDetailComponent } from './service-idi-detail/service-idi-detail.component';
 import { AyudasSubvencionesIdiDetailComponent } from './ayudas-subvenciones-idi-detail/ayudas-subvenciones-idi-detail.component';
-//import { FormacionWebinarsIdiComponent } from './formacion-webinars-idi/formacion-webinars-idi.component';
+
 import { FormacionAgendaIdiComponent } from './formacion-agenda-idi/formacion-agenda-idi.component';
 import { MenuHomeIdiComponent } from "./menu-home-idi/menu-home-idi-component";
 import { TransparenciaIdiComponent } from './transparencia-idi/transparencia-idi.component';
-//import { SectoresIdiComponent } from './sectores-idi/sectores-idi.component';
+
 import { ContentIdiComponent } from './content-idi/content-idi.component';
 import { I18nModule } from './i18n/i18n.module';
 import { SelectLanguageComponent } from './select-language/select-language.component';
@@ -36,7 +35,7 @@ import { FilterPipe } from './utils/filter.pipe';
 import { SearchTheWebResultListComponent } from './search-the-web-result-list/search-the-web-result-list.component';
 import { HighlightSearchTermPipe } from './utils/highlight-search-term.pipe';
 import { SearchTheWebDetailComponent } from './search-the-web-detail/search-the-web-detail.component';
-//import { MenuHomeEmprendedorDetailComponent } from './menu-home-emprendedor-detail/menu-home-emprendedor-detail.component';
+
 import { CategoryIdPipe } from './utils/category-id.pipe';
 import { ReempresaIdiComponent } from './reempresa-idi/reempresa-idi.component';
 import { ArticleDetailComponent } from './article-detail/article-detail.component';
@@ -59,67 +58,63 @@ import { HistoryServicesIdiComponent } from './history-services-idi/history-serv
 import { ProyectosIdiChildComponent } from './proyectos-idi-child/proyectos-idi-child.component';
 import { LevelTwoComponent } from './level-two/level-two.component';
 import { GetAliasPipe } from './Pipes/get-alias.pipe';
+import { TranslateModule } from "@ngx-translate/core";
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NoticiasComponent,
-    NoticiaDetailComponent,
-    MessagesComponent,
-    PageNotFoundComponent,
-    HomeComponentComponent,
-    //FeaturedIdiComponent,
-    AgendaIdiComponent,
-    //ServicesIdiComponent,
-    //FeaturedIdiDetailComponent,
-    ServiceIdiDetailComponent,
-    AyudasSubvencionesIdiDetailComponent,
-    //FormacionWebinarsIdiComponent,
-    FormacionAgendaIdiComponent,
-    MenuHomeIdiComponent,
-    TransparenciaIdiComponent,
-    //SectoresIdiComponent,
-    ContentIdiComponent,
-    SelectLanguageComponent,
-    MenuFooterIdiComponent,
-    ContactaIdiComponent,
-    NewsletterComponent,
-    SearchTheWebComponent,
-    FilterPipe,
-    SearchTheWebResultListComponent,
-    HighlightSearchTermPipe,
-    SearchTheWebDetailComponent,
-    //MenuHomeEmprendedorDetailComponent,
-    CategoryIdPipe,
-    ReempresaIdiComponent,
-    ArticleDetailComponent,
-    TransparencyDetailComponent,
-    FederDetailComponent,
-    ExploreIdiComponent,
-    ContactIdiHomeComponent,
-    MatrixIdiHomeComponent,
-    CategoryTitlePipe,
-    MatrixIdiHomeChildComponent,
-    LevelOneComponent,
-    WhoWeAreIdiComponent,
-    WhoWeAreIdiChildComponent,
-    ProyectosIdiComponent,
-    WellFormattedLinkPipe,
-    HistoryServicesIdiComponent,
-    ProyectosIdiChildComponent,
-    LevelTwoComponent,
-    GetAliasPipe
-  ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    I18nModule
-  ],
-  providers: [ {provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true} ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NoticiasComponent,
+        NoticiaDetailComponent,
+        MessagesComponent,
+        PageNotFoundComponent,
+        HomeComponentComponent,
+        AgendaIdiComponent,
+        ServiceIdiDetailComponent,
+        AyudasSubvencionesIdiDetailComponent,
+        FormacionAgendaIdiComponent,
+        MenuHomeIdiComponent,
+        TransparenciaIdiComponent,
+        ContentIdiComponent,
+        SelectLanguageComponent,
+        MenuFooterIdiComponent,
+        ContactaIdiComponent,
+        NewsletterComponent,
+        SearchTheWebComponent,
+        FilterPipe,
+        SearchTheWebResultListComponent,
+        HighlightSearchTermPipe,
+        SearchTheWebDetailComponent,
+        CategoryIdPipe,
+        ReempresaIdiComponent,
+        ArticleDetailComponent,
+        TransparencyDetailComponent,
+        FederDetailComponent,
+        ExploreIdiComponent,
+        ContactIdiHomeComponent,
+        MatrixIdiHomeComponent,
+        CategoryTitlePipe,
+        MatrixIdiHomeChildComponent,
+        LevelOneComponent,
+        WhoWeAreIdiComponent,
+        WhoWeAreIdiChildComponent,
+        ProyectosIdiComponent,
+        WellFormattedLinkPipe,
+        HistoryServicesIdiComponent,
+        ProyectosIdiChildComponent,
+        LevelTwoComponent,
+        GetAliasPipe
+    ],
+    providers: [{ provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true }],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        HttpClientModule,
+        I18nModule,
+        TranslateModule
+    ]
 })
 export class AppModule { 
   public allIDIContents: reqArticle[]
